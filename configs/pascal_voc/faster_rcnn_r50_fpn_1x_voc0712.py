10291d239ea1dbdc11b7c6b08ce8f0a8a7556581
tarun-munjal <munjaltarun01@gmail.com>
2020-06-12 01:18:26 +0530

    dfdsg

--- configs/pascal_voc/faster_rcnn_r50_fpn_1x_voc0712.py
+++ configs/pascal_voc/faster_rcnn_r50_fpn_1x_voc0712.py
@@ -1,11 +1,11 @@
  
 _base_ = [
-    '../_base_/models/faster_rcnn_r50_fpn.py', '../_base_/datasets/voc0712,
+    '../_base_/models/faster_rcnn_r50_fpn.py', '../_base_/datasets/voc0712',
     '../_base_/default_runtime.py'
 ]
 model = dict(roi_head=dict(bbox_head=dict(num_classes=4)))
 # optimizer
-optimizer = dict(type='SGD', lr=0.01, momentum=0.9, weight_decay=0.0001)
+optimizer = dict(type='SGD', lr=0.001, momentum=0.9, weight_decay=0.0001)
 optimizer_config = dict(grad_clip=None)
 # learning policy
 # actual epoch = 3 * 3 = 9

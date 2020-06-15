37fd0952e79ed7d35fc2828322dea708e2c2d108
tarun-munjal <munjaltarun01@gmail.com>
2020-06-15 11:07:58 +0530

    Third Try

--- configs/pascal_voc/faster_rcnn_r50_fpn_1x_voc0712.py
+++ configs/pascal_voc/faster_rcnn_r50_fpn_1x_voc0712.py
@@ -1,6 +1,8 @@
  
 _base_ = [
-    '../_base_/models/faster_rcnn_r50_fpn.py', '../_base_/datasets/voc0712.py',
+    # '../_base_/models/faster_rcnn_r50_fpn.py',
+	'../_base_/models/faster_rcnn_r50_caffe_c4.py',
+    '../_base_/datasets/voc0712.py',
     '../_base_/default_runtime.py'
 ]
 model = dict(roi_head=dict(bbox_head=dict(num_classes=10)))
--- mmdet/datasets/voc.py
+++ mmdet/datasets/voc.py
@@ -6,7 +6,7 @@ from .xml_style import XMLDataset
 @DATASETS.register_module()
 class VOCDataset(XMLDataset):
 
-    CLASSES = (   '0', '1',  '2', '3', '4', '5', '6', '7', '8', '9')
+    CLASSES = ('0', '1','2','3','4','5','6', '7', '8', '9')
 
     def __init__(self, **kwargs):
         super(VOCDataset, self).__init__(**kwargs)

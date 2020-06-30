8fcb7b0934203f32c7a7b1c546fc7bbf4c22b1d4
tarun-munjal <munjaltarun01@gmail.com>
2020-06-30 12:57:37 +0530

    dsfg

--- configs/pascal_voc/faster_rcnn_r50_fpn_1x_voc0712.py
+++ configs/pascal_voc/faster_rcnn_r50_fpn_1x_voc0712.py
@@ -11,6 +11,6 @@ optimizer = dict(type='SGD', lr=0.001, momentum=0.9, weight_decay=0.0001)
 optimizer_config = dict(grad_clip=None)
 # learning policy
 # actual epoch = 3 * 3 = 9
-lr_config = dict(policy='step', step=[4])
+lr_config = dict(policy='step', step=[6])
 # runtime settings
-total_epochs = 12  # actual epoch = 4 * 3 = 12
+total_epochs = 6  # actual epoch = 4 * 3 = 12
